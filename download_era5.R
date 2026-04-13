ac10e011ccc24950fef2cbfd86508846537fa031
Researchanalyst-J <jacob.tharakan@nhs.net>
2026-04-13 18:56:59 +0100

    Update download_era5.R

--- download_era5.R
+++ download_era5.R
@@ -1,14 +1,17 @@
 library(ecmwfr)
 library(googledrive)
 
-# 1. Log into Google Drive using your secret key
+# 1. Authenticate Copernicus (Directly inside R to fix the password error!)
+cds_key <- Sys.getenv("CDS_API_KEY")
+wf_set_key(user = "api", key = cds_key, service = "cds")
+
+# 2. Log into Google Drive using your secret key
 drive_auth(path = "gdrive_key.json")
 
-# 2. Check what is already inside your Google Drive
-# Make sure your folder in Google Drive is named exactly "ERA5_Data"
+# 3. Check what is already inside your Google Drive
 existing_files <- drive_ls("ERA5_Data")$name
 
-# 3. Figure out which 4-year batch is next
+# 4. Figure out which 4-year batch is next
 all_years <- 1940:2025
 year_batches <- split(all_years, ceiling(seq_along(all_years) / 4))
 
@@ -28,7 +31,7 @@ if (is.null(target_batch)) {
 
 print(paste("Downloading:", target_fname))
 
-# 4. Request the data from Copernicus
+# 5. Request the data from Copernicus
 request <- list(
   dataset_short_name = "reanalysis-era5-single-levels",
   product_type   = "reanalysis",
@@ -43,10 +46,9 @@ request <- list(
 )
 
 # Download it to the GitHub server
-# Because we built the .cdsapirc file in the YAML, we can just use user = "api"
 wf_request(user = "api", request = request, transfer = TRUE, path = ".", verbose = TRUE)
 
-# 5. Move it to Google Drive and delete it from GitHub
+# 6. Move it to Google Drive and delete it from GitHub
 print("Uploading to Google Drive...")
 drive_upload(target_fname, path = "ERA5_Data/")
 file.remove(target_fname)
