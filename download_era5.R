409154e9dbe7ada8924540a7a8c8538166921fa3
Researchanalyst-J <jacob.tharakan@nhs.net>
2026-04-14 13:03:44 +0100

    Update download_era5.R

--- download_era5.R
+++ download_era5.R
@@ -6,12 +6,10 @@ options(keyring_backend = "env")
 cds_key <- Sys.getenv("CDS_API_KEY")
 wf_set_key(user = "api", key = cds_key)
 
-# 2. Connect to Google Drive using the folder you just uploaded!
-# email = TRUE tells it to grab the slip out of the folder automatically
+# 2. Connect to Google Drive using your token
 drive_auth(cache = "gdrive_token", email = TRUE)
 
 # 3. Figure out which 4-year batch is next
-# Make sure your folder in Google Drive is named exactly "ERA5_Data"
 existing_files <- drive_ls("ERA5_Data")$name
 
 all_years <- 1940:2025
@@ -19,7 +17,8 @@ year_batches <- split(all_years, ceiling(seq_along(all_years) / 4))
 
 target_batch <- NULL
 for (batch in year_batches) {
-  fname <- paste0("era5_", min(batch), "_", max(batch), ".nc")
+  # I updated the name so you know it is UV and 24-hour data
+  fname <- paste0("era5_uv_24hr_", min(batch), "_", max(batch), ".nc")
   if (!(fname %in% existing_files)) {
     target_batch <- batch
     target_fname <- fname
@@ -37,11 +36,11 @@ print(paste("Downloading:", target_fname))
 request <- list(
   dataset_short_name = "reanalysis-era5-single-levels",
   product_type   = "reanalysis",
-  variable       = "surface_solar_radiation_downwards",
+  variable       = "downward_uv_radiation_at_the_surface", # Switched to UV!
   year           = as.character(target_batch),
   month          = sprintf("%02d", 1:12),
   day            = sprintf("%02d", 1:31),
-  time           = sprintf("%02d:00", 6:18),
+  time           = sprintf("%02d:00", 0:23), # All 24 hours!
   area           = c(-0.9, 29.3, -11.8, 40.5),
   format         = "netcdf",
   target         = target_fname
@@ -49,7 +48,7 @@ request <- list(
 
 wf_request(user = "api", request = request, transfer = TRUE, path = ".", verbose = TRUE)
 
-# 5. Move it to Google Drive and delete it from GitHub to save space
+# 5. Move it to Google Drive and delete it from GitHub
 print("Uploading to Google Drive...")
 drive_upload(target_fname, path = "ERA5_Data/")
 file.remove(target_fname)
