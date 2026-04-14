ef72b71a825f51801570b71e8c977018e34b07a7
Researchanalyst-J <jacob.tharakan@nhs.net>
2026-04-14 14:21:08 +0100

    Update download_era5.R

--- download_era5.R
+++ download_era5.R
@@ -7,13 +7,13 @@ wf_set_key(user = "api", key = Sys.getenv("CDS_API_KEY"))
 drive_auth(cache = "gdrive_token", email = TRUE)
 
 # Find next 2-year batch
-existing_files <- drive_ls("ERA5_Data_Malawi")$name
+existing_files <- drive_ls("ERA5_Data")$name
 all_years <- 1940:2025
 year_batches <- split(all_years, ceiling(seq_along(all_years) / 2))
 
 target_batch <- NULL
 for (batch in year_batches) {
-  fname <- paste0("era5_uv_daylight_malawi_", min(batch), "_", max(batch), ".nc")
+  fname <- paste0("era5_uv_daylight_tanzania_", min(batch), "_", max(batch), ".nc")
   if (!(fname %in% existing_files)) {
     target_batch <- batch
     target_fname <- fname
@@ -21,7 +21,7 @@ for (batch in year_batches) {
   }
 }
 
-if (is.null(target_batch)) stop("All Malawi data downloaded!")
+if (is.null(target_batch)) stop("All Tanzania data downloaded!")
 
 # SUBMIT THE REQUEST
 print(paste("Requesting batch:", target_fname))
@@ -32,11 +32,10 @@ request <- list(
   year           = as.character(target_batch),
   month          = sprintf("%02d", 1:12), day = sprintf("%02d", 1:31),
   time           = sprintf("%02d:00", 6:18), 
-  area           = c(-9.3, 32.6, -17.1, 35.9), 
+  area           = c(-0.9, 29.3, -11.8, 40.5), 
   format         = "netcdf", target = target_fname
 )
 
-# Use 'transfer = FALSE' so we can handle the waiting manually
 id <- wf_request(user = "api", request = request, transfer = FALSE)
 
 # MANUAL PATIENT POLLING LOOP
@@ -52,9 +51,9 @@ repeat {
   }
   
   print("Waiting 120 seconds before checking again...")
-  Sys.sleep(120) # This is the "secret sauce" - it stops the spamming
+  Sys.sleep(120) 
 }
 
 # Upload to Drive
-drive_upload(target_fname, path = "ERA5_Data_Malawi/")
+drive_upload(target_fname, path = "ERA5_Data/")
 file.remove(target_fname)
