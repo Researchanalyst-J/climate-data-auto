44f2fe4d8256937edc8eeddac055458b58492f9d
Researchanalyst-J <jacob.tharakan@nhs.net>
2026-04-25 22:35:34 +0100

    Update download_era5.R

--- download_era5.R
+++ download_era5.R
@@ -3,9 +3,7 @@ library(googledrive)
 
 options(keyring_backend = "env")
 wf_set_key(user = "api", key = Sys.getenv("CDS_API_KEY"))
-
-# Tell R to use the folder containing your newly rebuilt personal token
-drive_auth(cache = "gdrive_token", email = TRUE)
+drive_auth(cache = "gdrive_token", email = "jacobn158@gmail.com")
 
 # Focus strictly on the Tanzania folder
 existing_files <- drive_ls("ERA5_Data")$name
