a8637e0702fc3e48ffb06d2fe1fea6dd1ef7e46d
Researchanalyst-J <jacob.tharakan@nhs.net>
2026-04-25 21:27:05 +0100

    Update download_era5_malawi.R

--- download_era5_malawi.R
+++ download_era5_malawi.R
@@ -3,7 +3,9 @@ library(googledrive)
 
 options(keyring_backend = "env")
 wf_set_key(user = "api", key = Sys.getenv("CDS_API_KEY"))
-drive_auth(path = "gdrive_token/token_file")
+
+# Tell R to use the folder containing your newly rebuilt personal token
+drive_auth(cache = "gdrive_token", email = TRUE)
 
 # Focus strictly on the Malawi folder
 existing_files <- drive_ls("ERA5_Data_Malawi")$name
